bffd63d68d4e4ff6a251ec0127c450c7f6c82f03
Arthur Fernandes Vaz <arthurvaz1805@gmail.com>
2024-12-11 14:58:55 -0300

    Componente TECNICO UPDATE implementado

--- src/app/services/tecnico.service.ts
+++ src/app/services/tecnico.service.ts
@@ -10,6 +10,10 @@ import { Tecnico } from '../models/tecnico';
 export class TecnicoService {
 
   constructor(private http:HttpClient) { }
+
+  findById(id:any): Observable<Tecnico>{
+    return this.http.get<Tecnico>(`${API_CONFIG.baseUrl}/tecnicos/${id}`)
+  }
   
   findAll(): Observable<Tecnico[]> { 
     return this.http.get<Tecnico[]>(`${API_CONFIG.baseUrl}/tecnicos`)
@@ -17,5 +21,9 @@ export class TecnicoService {
 
   create(tecnico :Tecnico): Observable<Tecnico>{
     return this.http.post<Tecnico>(`${API_CONFIG.baseUrl}/tecnicos`, tecnico);
+  }
+
+  update (tecnico: Tecnico): Observable<Tecnico> {
+    return this.http.put<Tecnico>(`${API_CONFIG.baseUrl}/tecnicos/${tecnico.id}`, tecnico);
   }
 }
